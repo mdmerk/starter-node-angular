b65e4bedd62dfc589e684dcc1b7c8622eba7179f
Micah Merkel <micahmerkel@icloud.com>
2022-04-07 09:32:45 -0500

    adding deploy files and travis.yml file

--- deploy.js
+++ deploy.js
@@ -85,7 +85,7 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '',
+      host: '18.206.228.10',
       username: 'ubuntu',
       privateKey: 'hs-key.pem'
     })
